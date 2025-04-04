6cd1dd3058a7a32f60d0d537a8f982278b2b4ccd
Yetunde-Iyanu <awoladeyetunde60@gmail.com>
2025-04-04 05:38:11 +0100

    Higer Order Function

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 // FUNCTION ASSIGNMENT ;- Declaring Fullname//
 function fullname() {
-    console.log("Yetunde Dorcas Aeolade");
+    console.log("Yetunde Dorcas Awolade");
 }
 fullname ();
 
+function fullname(firstName,  lastName) {
+    return firstName + " " + lastName;
+}
+console.log(fullname("Yetunde", "Dorcas Awolade"));
 
+    //Decalaring Num//
+    function addNumbers(num1, num2) {
+        return num1 + num2
+    }
+    console.log(addNumbers(20, 13))
 
-    
+    //Higher Order Function //
+     const countries [
+        'Albania',
+        'Bolivia',
+        'Canada',
+        'Denmark',
+        'Ethiopia',
+        'Finlandd',
+        'Germany',
+        'Hungary',
+        'Iceland',
+        'Ireland',
+        'Japan',
+        'Kenya',
+     ]
